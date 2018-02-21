a10fdd7793d39b41b9609e5dbd695daec8782986
darthbeep <darthbeep@gmail.com>
2018-02-21 11:38:57 -0500

    working prototype started

--- mongo.pl
+++ mongo.pl
@@ -4,4 +4,17 @@ use warnings;
 use MongoDB;
 use 5.010;
 
+my $client = MongoDB->connect('homer.stuy.edu');
+my $db = $client->get_database('test');
+my $coll = $db->get_collection('restaurants');
+my $use = $coll->find;
+#my $use = $
+
+print $use;
+
+while (my $curr = $use->next) {
+    print $curr->{'borough'} . "\n";
+}
+
+
 print "hi\n";
