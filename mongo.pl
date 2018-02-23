0140b7acedeae0e66d1ca542871c97851e6b8c87
darthbeep <darthbeep@gmail.com>
2018-02-23 00:41:51 -0500

    some progress i guess

--- mongo.pl
+++ mongo.pl
@@ -12,6 +12,7 @@ my $use = $coll->find({'borough'=> 'Brooklyn'});
 
 #print $use;
 my @arr = $use->all;
+my @arr0 = $arr[0];
 my @first = $use->all;
 my @test = keys $arr[0];
 #for my $key (@test) {
@@ -20,7 +21,7 @@ my @test = keys $arr[0];
 #}
 
 my $num = 0;
-print $num;
+#print $num;
 
 #print $arr[0]->{'borough'}."\n";
 
@@ -31,32 +32,61 @@ print $num;
 #}
 
 sub display_object {
-    my (@object_take, $steps) = @_;
-    my @object = $object_take[0][0];
-    print keys $object[0];
-    #print "idk any more $steps $object \n";
-    #my @object_start = "@{ $object_take }";
-    #print $object_start;
-    #my @object_again = $object_start[0];
-    #print keys @object_again;
-    #my $object = $object[0];
-    #print keys $object2;
-    #print "\n---\n";
-    #print keys @object;
-    #print "\n";
-    #if (not ref(@object_again)) {
-        #print "$steps: $object \n";
-    #    return;
-    #}
-    #my @keys = keys @object;
-    #print @keys;
-    #for my $key (@keys) {
-    #    display_object($object->{$key}, $steps+1);
-    #}
+    my ($object_take, $steps) = @_;
+    #print "recieved as" . ref($object_take) . "\n";
+    #print ref($object_take) . "start\n";
+    #print $steps;
 
+    #print keys $object_start[0];
+
+    if (not ref($object_take) eq "ARRAY" and not ref($object_take) eq "HASH") {
+        print "$steps: $object_take\n";
+        return;
+        #print "idk";
+    }
+    print 'again: '. ref($object_take) . ",$object_take \n";
+    my @object_start = "";#@{ $object_take };;
+    if (ref($object_take) eq "HASH") {
+        @object_start = %{ $object_take };
+        print "keys:";
+        for my $thing (@object_start) {
+            display_object($thing, $steps+1);
+        }
+        return;
+    }
+    else {
+        @object_start = @{ $object_take };
+    }
+
+
+    my @object = $object_start[0];
+    print "\nkeys:\tand". ref($object[0]). "\n";
+    print keys @object;
+    #print "$object[0]\n";
+    my @keys;# = keys $object;
+    eval {
+        @keys = keys $object[0];
+    } or do {
+        @keys = keys @object;
+    };
+
+
+    print "keys printed: @keys\n";
+    for my $key (@keys) {
+        my @send = $object[0]->{$key};
+        #print "send: @send from $key as ". ref(@send) . "\n";
+        #print ref(@send) . "and the ref, @send\n";
+        if (ref(@send)) {
+            my @send2 = @send->all;
+            print "send2: @send2 ". ref(@send2). "\n";
+            display_object(\@send2->all, $steps+1);
+        }
+        else {
+            display_object(@send, $steps+1);
+        }
+    }
 }
 
 
-
-display_object(\@arr, 0);
-print "hi\n";
+display_object(\@arr0, 0);
+#print "hi\n";
